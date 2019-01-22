7bcd1263bf5a7e2be52ecfd809ab3e87f4f6322e
amaanooo <md.amanuddin@gmail.com>
2019-01-21 21:47:56 -0500

    fileseadded

---
+++ custom.js
@@ -0,0 +1,9 @@
+@import url('https://fonts.googleapis.com/css?family=Lato:400,700');
+body {
+
+
+    overflow-x: hidden;
+    font-family: 'Lato', sans-ServiceUIFrameContext;
+    color: #505962;
+
+}
