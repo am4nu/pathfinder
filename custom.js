ff848053de3e761e4583b1a86e83c32f17fbfcc5
amaanooo <md.amanuddin@gmail.com>
2019-01-24 02:31:38 -0500

    fixed alignment

--- custom.js
+++
@@ -1,9 +0,0 @@
-@import url('https://fonts.googleapis.com/css?family=Lato:400,700');
-body {
-
-
-    overflow-x: hidden;
-    font-family: 'Lato', sans-ServiceUIFrameContext;
-    color: #505962;
-
-}
